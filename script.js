3726b4c5f8266ed07f5a78540f30b38074184e5a
AvaShZmu <b3017h@gmail.com>
2025-02-19 23:04:59 +0700

    kill

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const images = [
 	"frieren5.jpg"
 ]
 
+const kill = "frierenkillyourself.jpg"
+const burgor = "burgor.jpg"
+
 let index = 0;
 
 function changeImage() {
@@ -15,3 +18,13 @@ function changeImage() {
 	}
 	document.getElementById("image").src = images[index]
 }
+document.getElementById("clickentity").addEventListener("click", function(){
+	let curr_img = document.getElementById("clickentity");
+	let curr_cap = document.getElementById("clickcaption");
+	curr_img.src = kill;
+	curr_cap.innerText = "Nigger don't interrupt her";
+	setTimeout(() => {
+		curr_img.src = burgor;
+		curr_cap.innerText = "Frieren eating";
+	}, 3000)
+});
